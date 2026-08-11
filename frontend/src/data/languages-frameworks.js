86485ba8fd268f05e4096987c055760e9cc3be82
Van Ngo <siunhan33@gmail.com>
2026-08-11 01:39:39 -0500

    Update language section

---
+++ frontend/src/data/languages-frameworks.js
@@ -0,0 +1,12 @@
+const languagesAndFrameworks = [
+    { name: 'CSS', icon: 'TbBrandCss3' },
+    { name: 'Javascript', icon: 'TbBrandJavascript' },
+    { name: 'HTML', icon: 'TbBrandHtml5' },
+    { name: 'Python', icon: 'TbBrandPython' },
+    { name: 'Java', icon: 'FaJava' },
+    { name: 'Node.js', icon: 'FaNodeJs' },
+    { name: 'Next.js', icon: 'TbBrandNextjs' },
+    { name: 'React.js', icon: 'TbBrandReact' },
+];
+
+export default languagesAndFrameworks;
---
+++ frontend/src/data/tools-infrastructure.js
@@ -0,0 +1,13 @@
+const devops = [
+    { name: 'Docker', icon: 'TbBrandDocker' },
+    { name: 'Kubernetes', icon: 'SiKubernetes' },
+    { name: 'GitHub Actions', icon: 'SiGithubactions' },
+    { name: 'GitHub', icon: 'TbBrandGithub' },
+    { name: 'Vercel', icon: 'TbBrandVercel' },
+    { name: 'Jira', icon: 'SiJira' },
+    { name: 'Confluence', icon: 'SiConfluence' },
+    { name: 'Playwright', icon: '' },
+    { name: 'Selenium', icon: 'SiSelenium' },
+];
+
+export default devops;
