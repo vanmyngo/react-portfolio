1dce72ca50a2b887e4a641e275f02dc295f936af
Van Ngo <siunhan33@gmail.com>
2026-08-13 20:42:11 -0500

    Update project and footer css

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -8,33 +8,24 @@ const ProjectCard = ({ project }) => {
                 alt={project.title}
                 className='project-card-image'
             />
-            <div xs='auto' className='project-card-header'>
-                <h2 className='project-card-title'><b>{project.title}</b></h2>
-                <div className='project-card-link'>
-                    <a 
-                        href={project.githubUrl}
-                        target='_blank'
-                        rel='noreferrer'
-                    >
-                        [ Github ]
-                    </a>
-                    <a
-                        href={project.liveDemoUrl}
-                        target='_blank'
-                        rel='noreferrer'
-                    >
-                        [ Live Demo ]
-                    </a>
-                </div>
+            <b>{project.title}</b>
+            {project.description}
+            <div className='project-card-link'>
+                <a 
+                    href={project.githubUrl}
+                    target='_blank'
+                    rel='noreferrer'
+                >
+                    [Github]
+                </a>
+                <a
+                    href={project.liveDemoUrl}
+                    target='_blank'
+                    rel='noreferrer'
+                >
+                    [Live Demo]
+                </a>
             </div>
-            <p className='project-card-description'>
-                OVERVIEW <br/>
-                {project.description}
-            </p>
-            <p className='project-card-technologies'>
-                TECH STACK <br/>
-                {project.technologies.join(', ')}
-            </p>
         </section>
     );
 };
